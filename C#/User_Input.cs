df7fb4a64af0f08b56848b0be7e25d8d8f144af2
TathagatBhatt <135730021+TathagatBhatt@users.noreply.github.com>
2023-11-06 21:18:21 +0530

    fix

---
+++ C#/User_Input.cs
@@ -0,0 +1,16 @@
+﻿//this is a simple input and out generator
+using System;
+class Program
+{
+    static void Main
+    {
+        // user gives input
+        Console.Write("Enter Your Name:");
+        string Name = Console.Readline();
+    //output greetings
+    Console.WriteLines("Hello"+Name+"!");
+        //closing window
+        Console.WriteLine("Press any key to exit....");
+        Console.ReadKey();
+    }
+}
